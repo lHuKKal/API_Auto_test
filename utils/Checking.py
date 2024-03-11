85abf85d06e3126d4210a21a401282b8942dbf1e
HuKKa <1wolfgang555@gmail.com>
2024-03-11 11:35:46 +0500

    Добавление новой проверки Extract

--- utils/Checking.py
+++ utils/Checking.py
@@ -48,15 +48,15 @@ class GetValue:
 
     """"Берем значение из указанного поля"""
     @staticmethod
-    def get_value_from_field(response: Response, extract_field_name):
+    def get_value_from_field(response: Response, get_field_name):
         check = response.json()
-        check_info = check.get(extract_field_name)
+        check_info = check.get(get_field_name)
         return check_info
 
     """"Берем значение указанного поля из указанного индекса объекта полученного списка"""
     @staticmethod
-    def get_value_from_item_of_list(response: Response, field_of_list, index, take_value_from_field):
+    def get_value_from_item_of_list(response: Response, field_of_list, index, get_value_from_field):
         response_check = response.json()
         first_index_value_from_list = response_check.get(field_of_list, [])[index]
-        take_value_from_field = first_index_value_from_list.get(take_value_from_field)
-        return take_value_from_field
+        get_value_from_field = first_index_value_from_list.get(get_value_from_field)
+        return get_value_from_field
